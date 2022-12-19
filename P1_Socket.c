95a5b3814c185073f39ed5a14b18d7a88add044d
bl0nder <goelpalaash@gmail.com>
2022-12-20 00:47:37 +0530

    testing random strings fr

--- P1_Socket.c
+++ P1_Socket.c
@@ -4,5 +4,18 @@
 
 int main() {
     
+    char* chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890-=[];',./"
+    char arr[50][5];
+
+    for (int i=0; i<50; i++) {
+        for (int j=0; j<5; j++) {
+            arr[i][j] = chars[(rand() % (72-0+1)) + 0];
+            printf("%c ", arr[i][j]);
+        }
+        printf("\n");
+    }
+
+
+
     return 0;
 }
