94807e86bd8557f4a5d070267c0f2152ee824ceb
bl0nder <goelpalaash@gmail.com>
2022-12-20 00:48:31 +0530

    .

--- P1_Socket.c
+++ P1_Socket.c
@@ -10,7 +10,7 @@ int main() {
     for (int i=0; i<50; i++) {
         for (int j=0; j<5; j++) {
             arr[i][j] = chars[(rand() % (72-0+1)) + 0];
-            printf("%c ", arr[i][j]);
+            printf("%c", arr[i][j]);
         }
         printf("\n");
     }
