0861f41fcf30ce350654df1e8435f49bb16b62c5
bl0nder <goelpalaash@gmail.com>
2022-12-20 13:30:18 +0530

    .

--- P1SocketServer.c
+++ P1SocketServer.c
@@ -16,14 +16,16 @@
 int main(int argc, char *argv[]) {
 
   char* chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
-  char arr[50][5];
+  char arr[50][6];
   for (int i=0; i<50; i++) {
       for (int j=0; j<5; j++) {
           arr[i][j] = chars[(rand() % (61-0+1)) + 0];
       }
+      arr[i][5] = '\0';
   }
 
   printf("TESTING: %s\n", arr[48]);
+  
 
   int randInd = (rand()%(30-0+1))+0;
   char highestInd[10];
