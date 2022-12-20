3988cdfd3bef73bbcfe852c7e42a1a344001b4d4
bl0nder <goelpalaash@gmail.com>
2022-12-20 13:43:44 +0530

    .

--- P1SocketClient.c
+++ P1SocketClient.c
@@ -43,7 +43,7 @@ int main(int argc, char *argv[]) {
     buffer[bytes_read] = '\0';
     strcpy(highestInd, buffer);
     printf("[CLIENT] Received string: %s\n", highestInd);
-    sleep(2);
+    // sleep(2);
   }
 
   
